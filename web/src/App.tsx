82768fd000bf2cb04e5e110d3c310b690b5ace95
Kevin Gomes <kevinmg50@gmail.com>
2026-09-20 14:57:30 -0300

    feat(app): add organization admin routes and related pages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { AppShell } from "./components/layout/AppShell";
-import { RequireAdmin, RequireAuth } from "./components/RequireAuth";
+import { RequireAdmin, RequireAuth, RequireOrgAdmin } from "./components/RequireAuth";
 import { LoginPage } from "./pages/LoginPage";
 import { DashboardPage } from "./pages/DashboardPage";
 import { ServerDetailPage } from "./pages/ServerDetailPage";
+import { EggsPage } from "./pages/EggsPage";
+import { MembersPage } from "./pages/MembersPage";
+import { AuditPage } from "./pages/AuditPage";
+import { OrgsPage } from "./pages/OrgsPage";
 import { UsersPage } from "./pages/UsersPage";
 
 export function App() {
@@ -12,31 +16,18 @@ export function App() {
       <Route path="/login" element={<LoginPage />} />
 
       <Route element={<RequireAuth />}>
-        <Route
-          path="/"
-          element={
-            <AppShell>
-              <DashboardPage />
-            </AppShell>
-          }
-        />
-        <Route
-          path="/servers/:namespace/:name"
-          element={
-            <AppShell>
-              <ServerDetailPage />
-            </AppShell>
-          }
-        />
+        <Route path="/" element={<AppShell><DashboardPage /></AppShell>} />
+        <Route path="/orgs/:org/servers/:name" element={<AppShell><ServerDetailPage /></AppShell>} />
+        <Route path="/eggs" element={<AppShell><EggsPage /></AppShell>} />
+        <Route path="/members" element={<AppShell><MembersPage /></AppShell>} />
+
+        <Route element={<RequireOrgAdmin />}>
+          <Route path="/audit" element={<AppShell><AuditPage /></AppShell>} />
+        </Route>
+
         <Route element={<RequireAdmin />}>
-          <Route
-            path="/users"
-            element={
-              <AppShell>
-                <UsersPage />
-              </AppShell>
-            }
-          />
+          <Route path="/orgs" element={<AppShell><OrgsPage /></AppShell>} />
+          <Route path="/users" element={<AppShell><UsersPage /></AppShell>} />
         </Route>
       </Route>
     </Routes>
