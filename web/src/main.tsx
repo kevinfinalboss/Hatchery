bc2a4f9bf4a6dfd0ccf43ca8824e20748b6c6cb0
Kevin Gomes <kevinmg50@gmail.com>
2026-09-20 17:12:06 -0300

    refactor(main): reorder providers for better structure in main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,6 +6,8 @@ import "./index.css";
 import { App } from "./App";
 import { AuthProvider } from "./lib/auth";
 import { OrgProvider } from "./lib/org";
+import { I18nProvider } from "./lib/i18n";
+import { ThemeProvider } from "./lib/theme";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: false, refetchOnWindowFocus: false } },
@@ -13,14 +15,18 @@ const queryClient = new QueryClient({
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AuthProvider>
-          <OrgProvider>
-            <App />
-          </OrgProvider>
-        </AuthProvider>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <ThemeProvider>
+      <I18nProvider>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <AuthProvider>
+              <OrgProvider>
+                <App />
+              </OrgProvider>
+            </AuthProvider>
+          </BrowserRouter>
+        </QueryClientProvider>
+      </I18nProvider>
+    </ThemeProvider>
   </StrictMode>,
 );
