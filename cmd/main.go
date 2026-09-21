1cf38bafb09821afc0bab37c98d2223d4e72de93
Kevin Gomes <kevinmg50@gmail.com>
2026-09-21 02:36:25 -0300

    feat(main): add Kubernetes clientset initialization in main function

--- cmd/main.go
+++ cmd/main.go
@@ -29,6 +29,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/kubernetes"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
@@ -180,10 +181,16 @@ func main() {
 		os.Exit(1)
 	}
 
+	clientset, err := kubernetes.NewForConfig(mgr.GetConfig())
+	if err != nil {
+		setupLog.Error(err, "Failed to create the Kubernetes clientset")
+		os.Exit(1)
+	}
 	if err := (&controller.GameServerReconciler{
 		Client:         mgr.GetClient(),
 		Scheme:         mgr.GetScheme(),
 		SFTPAgentImage: sftpAgentImage,
+		LogReader:      controller.ClientsetLogReader{Clientset: clientset},
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "Failed to create controller", "controller", "gameserver")
 		os.Exit(1)
