ce1d467b67919cefb8cb55aab53058d625cb7b0e
Kevin Gomes <kevinmg50@gmail.com>
2026-09-24 13:42:53 -0300

    feat(panelapi): add image policy route and enhance permissions handling

--- internal/panelapi/server.go
+++ internal/panelapi/server.go
@@ -39,6 +39,8 @@ type Server struct {
 
 	AllowedOrigins []string
 
+	AllowedImageRegistries []string
+
 	UIDir string
 
 	// Tickets stores single-use console tickets; LoginLimiter throttles failed
@@ -94,14 +96,20 @@ func (s *Server) Routes() http.Handler {
 		}
 		return s.requireOrgRole(min, inner)
 	}
-	// gsRoute is orgRoute for routes that act on one GameServer: a suspended server is locked for the
-	// organization (423 with the admin's reason); platform admins pass.
-	gsRoute := func(min paneldb.Role, action string, h http.HandlerFunc) http.Handler {
+
+	gsPermRoute := func(p paneldb.Permission, action string, h http.HandlerFunc) http.Handler {
 		var inner http.Handler = s.unlessSuspended(h)
 		if action != "" {
 			inner = s.audited(action, inner)
 		}
-		return s.requireOrgRole(min, inner)
+		return s.requireOrgRole(paneldb.RoleMember, s.requireServerPermission(p, inner))
+	}
+	gsAdminRoute := func(action string, h http.HandlerFunc) http.Handler {
+		var inner http.Handler = s.unlessSuspended(h)
+		if action != "" {
+			inner = s.audited(action, inner)
+		}
+		return s.requireOrgRole(paneldb.RoleAdmin, inner)
 	}
 	const org = "/api/v1/orgs/{org}"
 	const gs = org + "/gameservers/{name}"
@@ -117,7 +125,10 @@ func (s *Server) Routes() http.Handler {
 	mux.Handle("POST "+org+"/members", orgRoute(paneldb.RoleAdmin, "", s.handleAddMember))
 	mux.Handle("PATCH "+org+"/members/{userId}", orgRoute(paneldb.RoleAdmin, "", s.handleSetMemberRole))
 	mux.Handle("DELETE "+org+"/members/{userId}", orgRoute(paneldb.RoleMember, "", s.handleRemoveMember))
+	mux.Handle("GET "+org+"/members/{userId}/permissions", orgRoute(paneldb.RoleAdmin, "", s.handleGetMemberPermissions))
+	mux.Handle("PUT "+org+"/members/{userId}/permissions", orgRoute(paneldb.RoleAdmin, "", s.handlePutMemberPermissions))
 
+	mux.Handle("GET "+org+"/image-policy", orgRoute(paneldb.RoleMember, "", s.handleImagePolicy))
 	mux.Handle("GET "+org+"/eggs", orgRoute(paneldb.RoleMember, "", s.handleListOrgEggs))
 	mux.Handle("GET "+org+"/eggs/{scope}/{name}", orgRoute(paneldb.RoleMember, "", s.handleGetOrgEgg))
 	mux.Handle("POST "+org+"/eggs", orgRoute(paneldb.RoleAdmin, "", s.handleCreateOrgEgg))
@@ -131,39 +142,45 @@ func (s *Server) Routes() http.Handler {
 
 	mux.Handle("GET "+org+"/gameservers", orgRoute(paneldb.RoleMember, "", s.handleListGameServers))
 	mux.Handle("POST "+org+"/gameservers", orgRoute(paneldb.RoleAdmin, "", s.handleCreateGameServer))
-	mux.Handle("GET "+gs, orgRoute(paneldb.RoleMember, "", s.handleGetGameServer))
-	mux.Handle("PATCH "+gs, gsRoute(paneldb.RoleAdmin, "gameserver.update", s.handleUpdateGameServer))
-	mux.Handle("DELETE "+gs, gsRoute(paneldb.RoleAdmin, "gameserver.delete", s.handleDeleteGameServer))
-	mux.Handle("PATCH "+gs+"/state", gsRoute(paneldb.RoleMember, "gameserver.state", s.handleSetGameServerState))
+	mux.Handle("GET "+gs, s.requireOrgRole(paneldb.RoleMember, s.requireServerPermission("", http.HandlerFunc(s.handleGetGameServer))))
+	mux.Handle("PATCH "+gs, gsAdminRoute("gameserver.update", s.handleUpdateGameServer))
+	mux.Handle("DELETE "+gs, gsAdminRoute("gameserver.delete", s.handleDeleteGameServer))
+	mux.Handle("PATCH "+gs+"/state", gsPermRoute(paneldb.PermPower, "gameserver.state", s.handleSetGameServerState))
 	mux.Handle("POST "+gs+"/suspend", s.requireAdmin(orgRoute(paneldb.RoleOwner, "gameserver.suspend", s.handleSuspendGameServer)))
 	mux.Handle("POST "+gs+"/unsuspend", s.requireAdmin(orgRoute(paneldb.RoleOwner, "gameserver.unsuspend", s.handleUnsuspendGameServer)))
 	mux.Handle("GET "+org+"/backup-settings", orgRoute(paneldb.RoleMember, "", s.handleGetBackupSettings))
 	mux.Handle("PUT "+org+"/backup-connections/{connection}", orgRoute(paneldb.RoleAdmin, "", s.handlePutBackupConnection))
 	mux.Handle("DELETE "+org+"/backup-connections/{connection}", orgRoute(paneldb.RoleAdmin, "", s.handleDeleteBackupConnection))
-	mux.Handle("GET "+gs+"/backups", gsRoute(paneldb.RoleMember, "", s.handleListBackups))
-	mux.Handle("POST "+gs+"/backups", gsRoute(paneldb.RoleAdmin, "backup.create", s.handleCreateBackup))
-	mux.Handle("DELETE "+gs+"/backups/{backup}", gsRoute(paneldb.RoleAdmin, "backup.delete", s.handleDeleteBackup))
-	mux.Handle("POST "+gs+"/backups/{backup}/restore", gsRoute(paneldb.RoleAdmin, "backup.restore", s.handleRestoreBackup))
-	mux.Handle("POST "+gs+"/reinstall", gsRoute(paneldb.RoleAdmin, "gameserver.reinstall", s.handleReinstallGameServer))
-	mux.Handle("POST "+gs+"/restart", gsRoute(paneldb.RoleMember, "gameserver.restart", s.handleRestartGameServer))
-	mux.Handle("GET "+gs+"/metrics", gsRoute(paneldb.RoleMember, "", s.handleMetrics))
-	mux.Handle("GET "+gs+"/runtime", gsRoute(paneldb.RoleMember, "", s.handleRuntime))
-	mux.Handle("GET "+gs+"/logs", gsRoute(paneldb.RoleMember, "", s.handleLogs))
-	mux.Handle("POST "+gs+"/sftp-session", gsRoute(paneldb.RoleMember, "gameserver.sftp-session", s.handleSFTPSession))
+	mux.Handle("GET "+gs+"/backups", gsPermRoute(paneldb.PermBackupsRead, "", s.handleListBackups))
+	mux.Handle("POST "+gs+"/backups", gsPermRoute(paneldb.PermBackupsManage, "backup.create", s.handleCreateBackup))
+	mux.Handle("DELETE "+gs+"/backups/{backup}", gsPermRoute(paneldb.PermBackupsManage, "backup.delete", s.handleDeleteBackup))
+	mux.Handle("POST "+gs+"/backups/{backup}/restore", gsPermRoute(paneldb.PermBackupsManage, "backup.restore", s.handleRestoreBackup))
+	mux.Handle("GET "+gs+"/schedules", gsPermRoute(paneldb.PermSchedules, "", s.handleListSchedules))
+	mux.Handle("POST "+gs+"/schedules", gsPermRoute(paneldb.PermSchedules, "schedule.create", s.handleCreateSchedule))
+	mux.Handle("GET "+gs+"/schedules/{schedule}", gsPermRoute(paneldb.PermSchedules, "", s.handleGetSchedule))
+	mux.Handle("PUT "+gs+"/schedules/{schedule}", gsPermRoute(paneldb.PermSchedules, "schedule.update", s.handleUpdateSchedule))
+	mux.Handle("DELETE "+gs+"/schedules/{schedule}", gsPermRoute(paneldb.PermSchedules, "schedule.delete", s.handleDeleteSchedule))
+	mux.Handle("POST "+gs+"/schedules/{schedule}/run", gsPermRoute(paneldb.PermSchedules, "schedule.run", s.handleRunSchedule))
+	mux.Handle("POST "+gs+"/reinstall", gsAdminRoute("gameserver.reinstall", s.handleReinstallGameServer))
+	mux.Handle("POST "+gs+"/restart", gsPermRoute(paneldb.PermPower, "gameserver.restart", s.handleRestartGameServer))
+	mux.Handle("GET "+gs+"/metrics", gsPermRoute("", "", s.handleMetrics))
+	mux.Handle("GET "+gs+"/runtime", gsPermRoute("", "", s.handleRuntime))
+	mux.Handle("GET "+gs+"/logs", gsPermRoute(paneldb.PermConsoleRead, "", s.handleLogs))
+	mux.Handle("POST "+gs+"/sftp-session", gsPermRoute(paneldb.PermFilesWrite, "gameserver.sftp-session", s.handleSFTPSession))
 
-	mux.Handle("GET "+gs+"/files", gsRoute(paneldb.RoleMember, "", s.handleListFiles))
-	mux.Handle("GET "+gs+"/files/content", gsRoute(paneldb.RoleMember, "", s.handleGetFileContent))
-	mux.Handle("PUT "+gs+"/files/content", gsRoute(paneldb.RoleMember, "file.write", s.handlePutFileContent))
-	mux.Handle("POST "+gs+"/files/mkdir", gsRoute(paneldb.RoleMember, "file.mkdir", s.handleMkdir))
-	mux.Handle("POST "+gs+"/files/rename", gsRoute(paneldb.RoleMember, "file.rename", s.handleRenameFile))
-	mux.Handle("POST "+gs+"/files/delete", gsRoute(paneldb.RoleMember, "file.delete", s.handleDeleteFiles))
-	mux.Handle("POST "+gs+"/files/copy", gsRoute(paneldb.RoleMember, "file.copy", s.handleCopyFile))
-	mux.Handle("POST "+gs+"/files/upload", gsRoute(paneldb.RoleMember, "file.upload", s.handleUploadFile))
-	mux.Handle("GET "+gs+"/files/download", gsRoute(paneldb.RoleMember, "", s.handleDownloadFiles))
-	mux.Handle("POST "+gs+"/files/compress", gsRoute(paneldb.RoleMember, "file.compress", s.handleCompressFiles))
-	mux.Handle("POST "+gs+"/files/decompress", gsRoute(paneldb.RoleMember, "file.decompress", s.handleDecompressFile))
+	mux.Handle("GET "+gs+"/files", gsPermRoute(paneldb.PermFilesRead, "", s.handleListFiles))
+	mux.Handle("GET "+gs+"/files/content", gsPermRoute(paneldb.PermFilesRead, "", s.handleGetFileContent))
+	mux.Handle("PUT "+gs+"/files/content", gsPermRoute(paneldb.PermFilesWrite, "file.write", s.handlePutFileContent))
+	mux.Handle("POST "+gs+"/files/mkdir", gsPermRoute(paneldb.PermFilesWrite, "file.mkdir", s.handleMkdir))
+	mux.Handle("POST "+gs+"/files/rename", gsPermRoute(paneldb.PermFilesWrite, "file.rename", s.handleRenameFile))
+	mux.Handle("POST "+gs+"/files/delete", gsPermRoute(paneldb.PermFilesWrite, "file.delete", s.handleDeleteFiles))
+	mux.Handle("POST "+gs+"/files/copy", gsPermRoute(paneldb.PermFilesWrite, "file.copy", s.handleCopyFile))
+	mux.Handle("POST "+gs+"/files/upload", gsPermRoute(paneldb.PermFilesWrite, "file.upload", s.handleUploadFile))
+	mux.Handle("GET "+gs+"/files/download", gsPermRoute(paneldb.PermFilesRead, "", s.handleDownloadFiles))
+	mux.Handle("POST "+gs+"/files/compress", gsPermRoute(paneldb.PermFilesWrite, "file.compress", s.handleCompressFiles))
+	mux.Handle("POST "+gs+"/files/decompress", gsPermRoute(paneldb.PermFilesWrite, "file.decompress", s.handleDecompressFile))
 
-	mux.Handle("POST "+gs+"/console-ticket", gsRoute(paneldb.RoleMember, "gameserver.console-ticket", s.handleConsoleTicket))
+	mux.Handle("POST "+gs+"/console-ticket", gsPermRoute(paneldb.PermConsoleWrite, "gameserver.console-ticket", s.handleConsoleTicket))
 	mux.Handle("GET "+gs+"/console", s.requireConsoleTicket(http.HandlerFunc(s.handleConsole)))
 
 	mux.Handle("GET "+org+"/audit", orgRoute(paneldb.RoleAdmin, "", s.handleListOrgAudit))
